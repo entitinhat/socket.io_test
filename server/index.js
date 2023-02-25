0fc55b7d6e979e111f246ac0ea4c11c3ae47245b
nhatktvn2001 <nhatktvn2001@gmail.com>
2023-02-25 14:38:23 +0700

    socket id testing

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const { Server } = require('socket.io')
+const cors = require('cors')
+
+app.use(cors())
+
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000", // where your frontend runs
+        methods: ["GET", "POST"],
+    },
+})
+
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`)
+})
+
+server.listen(5000, () => {
+    console.log('your server is ready')
+})
